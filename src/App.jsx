866834ee454d02335c8ca0dce0da627970eab37e
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-11-02 12:09:22 +0600

    new project and added login and signup pages

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Home from "./Pages/Home";
+import "./style.scss";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        {/* <Register /> */}
+        {/* <Login /> */}
+        <Home />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Pages/Register.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Add from "../img/addAvatar.png";
+
+const Register = () => {
+  return (
+    <>
+      <div className="formContainer">
+        <div className="formWrapper">
+          <span className="logo">Lama chat</span>
+          <span className="title">Register</span>
+          <form>
+            <input type="text" placeholder="display name" />
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <input type="file" id="file" style={{ display: "none" }} />
+            <label htmlFor="file">
+              <img src={Add} alt="" />
+              <span>Add and avatar</span>
+            </label>
+            <button>Sign Up</button>
+          </form>
+          <p>You already have an account? Login</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Register;
